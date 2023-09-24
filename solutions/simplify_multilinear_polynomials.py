3df4f9c479207dd9a3747b3a989883fd5303acc0
buzbohdan <buzbohdan@gmail.com>
2023-09-24 11:10:14 +0300

    Finish multilinear polynomials simplification

--- solutions/simplify_multilinear_polynomials.py
+++ solutions/simplify_multilinear_polynomials.py
@@ -2,37 +2,54 @@
 
 from collections import defaultdict
 
-def simplify(poly: str):
-    number_start_index = None
-    symbols_start_index = None
-    current_sign = 1
-    symbols_grouped = defaultdict(int)
-    END_SYMBOL = '|'
-    for index, char in enumerate(poly + END_SYMBOL):
+# A special symbol that will be added to the end of the polynomial to simplify
+# logic of processing the last element
+END_SYMBOL = "|"
 
-        if char in ['+', '-', END_SYMBOL]:
-            new_sign = 1 if char == '+' else -1
+
+def simplify(poly: str) -> str:
+    number_start_index: int | None = None
+    symbols_start_index: int | None = None
+    sign: str | None = None
+    coefficients: dict[str, int] = defaultdict(int)
+
+    for index, char in enumerate(poly + END_SYMBOL):
+        if char in ["+", "-", END_SYMBOL]:
             if index == 0:
-                current_sign = new_sign
+                sign = char
                 continue
 
             if number_start_index is not None:
-                number = int(poly[number_start_index:symbols_start_index]) * current_sign
+                coefficient = int(poly[number_start_index:symbols_start_index])
             else:
-                number = current_sign
+                coefficient = 1
+            if sign == "-":
+                coefficient = -coefficient
 
-            symbols = ''.join(sorted(poly[symbols_start_index:index]))
-            symbols_grouped[symbols] += number
+            symbols = "".join(sorted(poly[symbols_start_index:index]))
+            coefficients[symbols] += coefficient
 
             number_start_index = None
             symbols_start_index = None
-            current_sign = new_sign
-        if char.isnumeric():
-            number_start_index = number_start_index or index
-        if char.isalpha():
-            symbols_start_index = symbols_start_index or index
-    return symbols_grouped
+            sign = char
 
+        if char.isnumeric() and number_start_index is None:
+            number_start_index = index
 
-res = simplify('-a+5ab+3a-c-2a')
-assert res == '5b-a'
+        if char.isalpha() and symbols_start_index is None:
+            symbols_start_index = index
+
+    result_fragments: list[str] = []
+
+    # Sort all symbols by number of variables and then in the lexigraphic order
+    sorted_symbols = sorted(coefficients, key=lambda symbols: (len(symbols), symbols))
+    for symbols in sorted_symbols:
+        coefficient = coefficients[symbols]
+        if coefficient == 0:
+            continue
+
+        sign = "-" if coefficient < 0 else ("" if not result_fragments else "+")
+        number_in_fragment = abs(coefficient) if abs(coefficient) != 1 else ""
+        result_fragments.append(f"{sign}{number_in_fragment}{symbols}")
+
+    return "".join(result_fragments)
